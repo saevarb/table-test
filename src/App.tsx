da1dc771e70b2913fdb5688e46d4b2a1bd42e1ea
Sævar Berg <icedane@gmail.com>
2020-06-04 16:24:21 +0200

    Initial commit with subtable

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,193 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useMemo, useCallback } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import payload from "./payload.json";
+import { ContractOverview, Contract, Joined } from "./types";
+import { useTable, Column, useExpanded, Row } from "react-table";
+
+const rawPayload: ContractOverview[] = payload as any;
+
+type RowType = ContractOverview;
+
+let testPayload: RowType[] = [];
+
+for (let i = 0; i < 10; i++) {
+  testPayload.push(rawPayload[0]);
+}
+
+console.log(testPayload);
+
+// "customerRpi": "2781",
+// "companyCodeName": "itelligence DK",
+// "companyCodeNumber": "1200",
+const overviewColDefs: Column<RowType>[] = [
+  {
+    Header: "Customer",
+    id: "customer_header",
+    columns: [
+      { Header: "Name", accessor: "customerName" },
+      { Header: "RPI", accessor: "customerRpi" }
+    ]
+  },
+  {
+    Header: "Company",
+    id: "company_header",
+    columns: [
+      { Header: "Code name", accessor: "companyCodeName" },
+      { Header: "Code number", accessor: "companyCodeNumber" }
+    ]
+  }
+];
+
+const contractColDefs: Column<Contract>[] = [
+  { Header: "RPI", accessor: "contractRpi" },
+  { Header: "Sales rep.", accessor: "salesResponsible" },
+  { Header: "Type", accessor: "contractTypeCode" },
+  { Header: "Invoice cycle", accessor: "invoicingCycle" },
+  { Header: "Revenue", accessor: "totalRevenue" },
+  { Header: "Cost", accessor: "totalCost" },
+  { Header: "Product Type", accessor: "productType" }
+];
+
+interface ContractTableProps {
+  contracts: Contract[];
+}
+
+const ContractTable = (props: ContractTableProps) => {
+  console.log(props.contracts);
+  const data = useMemo(() => props.contracts, [props]);
+  const cols = useMemo(() => contractColDefs, []);
+
+  const {
+    headerGroups,
+    prepareRow,
+    getTableProps,
+    getTableBodyProps,
+    rows
+  } = useTable<Contract>({
+    columns: cols,
+    data: data
+  });
+
+  return (
+    <table {...getTableProps()}>
+      <thead>
+        {headerGroups.map(hg => (
+          <tr {...hg.getHeaderGroupProps()}>
+            {hg.headers.map(col => (
+              <th {...col.getHeaderProps()}>
+                <div>{col.render("Header")}</div>
+              </th>
+            ))}
+          </tr>
+        ))}
+      </thead>
+      <tbody {...getTableBodyProps()}>
+        {rows.map(row => {
+          prepareRow(row);
+          return (
+            <tr
+              className={
+                row.original.contractStatus === "Active"
+                  ? "contract-active"
+                  : "contract-inactive"
+              }
+              {...row.getRowProps()}
+            >
+              {row.cells.map(cell => (
+                <td {...cell.getCellProps()}>{cell.render("Cell")}</td>
+              ))}
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+};
+
+const OverviewTable = () => {
+  const cols = useMemo(() => overviewColDefs, []);
+  const data = useMemo(() => testPayload, []);
+
+  const {
+    headerGroups,
+    prepareRow,
+    getTableProps,
+    getTableBodyProps,
+    rows,
+    visibleColumns
+  } = useTable<RowType>(
+    {
+      columns: cols,
+      data: data,
+      initialState: {
+        expanded: {
+          "0": true
+        },
+        hiddenColumns: [
+          // "contract_header",
+          // "order_header",
+          // "product_header",
+          // "contractRpi",
+          // "salesResponsible"
+        ]
+      }
+    },
+    useExpanded
+  );
+
+  const renderSub = useCallback((row: Row<RowType>) => {
+    return <ContractTable contracts={row.original.contracts}></ContractTable>;
+  }, []);
+  // state.hiddenColumns
+
+  return (
+    <table {...getTableProps()}>
+      <thead>
+        {headerGroups.map(hg => (
+          <tr {...hg.getHeaderGroupProps()}>
+            {hg.headers.map(col => (
+              <th {...col.getHeaderProps()}>
+                <div>{col.render("Header")}</div>
+              </th>
+            ))}
+          </tr>
+        ))}
+      </thead>
+      <tbody {...getTableBodyProps()}>
+        {rows.map(row => {
+          prepareRow(row);
+          return (
+            <>
+              <tr
+                {...{
+                  ...row.getRowProps(),
+                  ...row.getToggleRowExpandedProps()
+                }}
+              >
+                <>
+                  {row.cells.map(cell => (
+                    <td {...cell.getCellProps()}>{cell.render("Cell")}</td>
+                  ))}
+                </>
+              </tr>
+              {row.isExpanded ? (
+                <tr>
+                  <td colSpan={visibleColumns.length}>{renderSub(row)}</td>
+                </tr>
+              ) : null}
+            </>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <OverviewTable></OverviewTable>
     </div>
   );
 }
---
+++ src/types.ts
@@ -0,0 +1,108 @@
+export type Joined = ContractOverview &
+  Partial<Contract> &
+  Partial<Order> &
+  Partial<Product>;
+export interface ContractOverview {
+  customerName: string;
+  customerRpi: string;
+  companyCodeName: string;
+  companyCodeNumber: string;
+  contracts: Contract[];
+  thirdPartyNumbers: ThirdPartyNumber[];
+}
+
+export interface Contract {
+  contractRpi: number;
+  salesResponsible: string;
+  contractTypeCode: string;
+  totalRevenue: string;
+  totalCost: string;
+  nextBillingDate: Date | null;
+  invoicingCycle: InvoicingCycle;
+  productType: string;
+  contractStatus: string;
+  orders: Order[];
+  products: Product[];
+  totalRevenueBigDecimal: number;
+  totalCostBigDecimal: number;
+}
+
+export enum InvoicingCycle {
+  OneTime = "One time",
+  Quarterly = "Quarterly"
+}
+
+export interface Order {
+  orderStartDate: Date;
+  orderEndDate: Date;
+  contractDuration: null;
+  customerNoticePeriod: number;
+  customerPo: null;
+  quantity: number;
+  customerSignatureDate: Date;
+  quoteNumber: null;
+  quoteConfirmation: null;
+  vendorSalesOrderNumber: null;
+  vendorNoticePeriod: null;
+  nextBillingDate: null;
+  customerSupportLevelCode: null;
+  vendorSupportLevelCode: null;
+  wbsNumber: string;
+  orderRevenueAmount: string;
+  orderCostAmount: string;
+  costRevenueDetails: CostRevenueDetail[];
+  statuses: Status[];
+  regulations: any[];
+  orderRevenueAmountBigDecimal: number;
+  orderCostAmountBigDecimal: number;
+}
+
+export interface CostRevenueDetail {
+  type: Type;
+  percentage: number;
+  licenseAmount: string;
+  maintenanceBaseAmount: string;
+  fee: string;
+  totalContractAmount: string;
+  currencyCode: CurrencyCode;
+  invoicingDate: null;
+  licenseAmountBigDecimal: number | null;
+  maintenanceBaseAmountBigDecimal: number | null;
+  invoicingCyclePeriodName: InvoicingCycle;
+  totalContractAmountBigDecimal: null;
+  feeBigDecimal: null;
+}
+
+export enum CurrencyCode {
+  Dkk = "DKK",
+  Eur = "EUR"
+}
+
+export enum Type {
+  Cost = "Cost",
+  Revenue = "Revenue"
+}
+
+export interface Status {
+  type: string;
+  name: string;
+  description: null;
+}
+
+export interface Product {
+  name: string;
+  productType: string;
+  vendorName: string;
+  vendorRpiNumber: number;
+  materials: Material[];
+}
+
+export interface Material {
+  description: string;
+  licenseKey: null;
+}
+
+export interface ThirdPartyNumber {
+  name: string;
+  number: string;
+}
